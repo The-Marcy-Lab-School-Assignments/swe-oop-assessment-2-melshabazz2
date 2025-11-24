41b4b0cd8ef5236c59413848eb5cdb850f892a80
melshabazz2 <melshabazz2@gmail.com>
2025-11-24 15:57:35 -0500

    completed

--- src/phone.js
+++ src/phone.js
@@ -1,12 +1,81 @@
 class Phone {
+    brand;
+    model;
+    #password
+    batteryLevel = 100
+    constructor(brand, model, password) {
+        this.brand = brand
+        this.model = model
+        this.#password = password
+
+    }
+    makeCall(number) {
+        this.batteryLevel -= 5
+        return `Calling ${number}`
+    }
+    charge() {
+        this.batteryLevel = 100
+        return `Phone fully charged`
+    }
+    unlock(password) {
+        return password === this.#password
+    }
 
 }
 
-class iPhone {
+class iPhone extends Phone {
+    constructor(model, password, numberOfCameras) {
+        super('Apple', model, password)
+        this.numberOfCameras = numberOfCameras
+    }
+    makeCall(number) {
+        this.batteryLevel -= 5
+        return `Calling ${number} using FaceTime audio`
+    }
+    faceTime(name) {
+        this.batteryLevel -= 10
+        return `Facetiming ${name}`
+    }
 
 }
 
 // TEST YOUR CODE HERE
+const flipPhone = new Phone("Nokia", "Flip", "TimeToLearn882");
+console.log(flipPhone); // Phone { brand: "Nokia", model: "Flip", batteryLevel: 100 }
+
+// 1. Making a call and checking battery
+console.log(flipPhone.makeCall("123-456-7890")); // Calling 123-456-7890
+console.log(flipPhone.batteryLevel); // 95
+
+// 2. Charging and checking battery
+console.log(flipPhone.charge()); // Phone fully charged
+console.log(flipPhone.batteryLevel); // 100
+
+// 3. Unlocking the phone
+console.log(flipPhone.unlock("TimeToLearn882")); // true
+console.log(flipPhone.unlock("TimeToLearn883")); // false
+
+
+const smartPhone = new iPhone("iPhone 14 Pro", "TimeToLearn882", 3);
+
+console.log(smartPhone); // iPhone { brand: "Apple", model: "iPhone 14 Pro", numberOfCameras: 3, batteryLevel: 100}
+
+// 1. Making a call 
+console.log(smartPhone.makeCall("555-1234")); // Calling 555-1234 using FaceTime audio
+console.log(smartPhone.batteryLevel); // Should be 95
+
+// 2. Using the faceTime method
+console.log(smartPhone.faceTime("Alice")); // Facetiming Alice
+console.log(smartPhone.batteryLevel); // Should be 85
+
+// 3. Charge the phone
+console.log(smartPhone.charge()); // Phone fully charged
+console.log(smartPhone.batteryLevel); // Should be 100
+
+// 4. Unlock the phone
+console.log(smartPhone.unlock("0000")); // Invalid password
+console.log(smartPhone.unlock("TimeToLearn882")); // true
+
 
 
 // DO NOT REMOVE
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,4 +1,40 @@
 class Pokemon {
+    static allPokemon = []
+    #health = 100
+    #level = 1
+    constructor(name, type) {
+        this.name = name
+        this.type = type
+        Pokemon.allPokemon.push(this)
+    }
+    get health() {
+        return this.#health
+    }
+    get level() {
+        return this.#level
+    }
+    levelUp() {
+        this.#level++
+        this.#health += 10
+        console.log(`${this.name} leveled up to level ${this.#level}!`)
+    }
+    isFainted() {
+        if (this.#health <= 0) {
+            return true.mnnnnn
+        } else {
+            return false
+        }
+    }
+    attack(targetPokemon) {
+        targetPokemon.#health -= 10 * this.#level
+        console.log(`${this.name} attacked ${targetPokemon}!`)
+    }
+    static getTotalPokemon() {
+        return Pokemon.allPokemon.length
+    }
+    static findByName(name) {
+        return Pokemon.allPokemon.find(pokemon => pokemon.name === name)
+    }
 
 }
 
